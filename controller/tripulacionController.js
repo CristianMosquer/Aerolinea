8be44a8a9e3f5a7c0292f06f810f9a8c9ea565b1
CristianMosquer <cristiansantoplay2@gmail.com>
2024-12-03 21:10:44 -0500

    cambios2

--- controller/tripulacionController.js
+++ controller/tripulacionController.js
@@ -1,43 +1,52 @@
-const Tripulacion = require('../models/Tripulacion');
+const Tripulacion = require("../models/tripulacion");
 
 const obtenerTripulacion = async (req, res) => {
-    try {
-        const tripulacion = await Tripulacion.find().populate('id_vuelo');
-        res.json(tripulacion);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+  try {
+    const tripulacion = await Tripulacion.find().populate("id_vuelo");
+    res.json(tripulacion);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const crearTripulante = async (req, res) => {
-    try {
-        const tripulante = new Tripulacion(req.body);
-        const tripulanteGuardado = await tripulante.save();
-        res.status(201).json(tripulanteGuardado);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
+  try {
+    const tripulante = new Tripulacion(req.body);
+    const tripulanteGuardado = await tripulante.save();
+    res.status(201).json(tripulanteGuardado);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
 
 const actualizarTripulante = async (req, res) => {
-    try {
-        const tripulanteActualizado = await Tripulacion.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true,
-        });
-        res.json(tripulanteActualizado);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
+  try {
+    const tripulanteActualizado = await Tripulacion.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    res.json(tripulanteActualizado);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
 
 const eliminarTripulante = async (req, res) => {
-    try {
-        await Tripulacion.findByIdAndDelete(req.params.id);
-        res.json({ message: 'Tripulante eliminado correctamente' });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+  try {
+    await Tripulacion.findByIdAndDelete(req.params.id);
+    res.json({ message: "Tripulante eliminado correctamente" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
-module.exports = { obtenerTripulacion, crearTripulante, actualizarTripulante, eliminarTripulante };
+module.exports = {
+  obtenerTripulacion,
+  crearTripulante,
+  actualizarTripulante,
+  eliminarTripulante,
+};
