28d4cb2ce4aecea2b73dae7c27ded076f1bf80b5
CristianMosquer <160748974+CristianMosquer@users.noreply.github.com>
2024-12-10 22:56:23 -0500

    Update Vuelo.js

--- models/Vuelo.js
+++ models/Vuelo.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
 
-const VueloSchema = new mongoose.Schema({
+const vueloSchema = new mongoose.Schema({
   fecha: { type: Date, required: true },
   destino: { type: String, required: true },
   origen: { type: String, required: true },
   matricula: { type: String, required: true },
 });
 
-const Vuelo = mongoose.model("Vuelo", VueloSchema, "Aerolinea.Aerolineas"); // Asegúrate de que la colección sea la correcta
-
-module.exports = Vuelo;
+module.exports = mongoose.model("Vuelo", vueloSchema);
